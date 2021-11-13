1c4517f87eca3c92c0af42faa5d94d8ad313b793
VShev <viky2503@gmail.com>
2021-11-14 00:58:30 +0200

    music

--- YouAreNotTheMainHero/Assets/Scripts/WindowsManager.cs
+++ YouAreNotTheMainHero/Assets/Scripts/WindowsManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using Assets.Scripts;
 using UnityEngine.UI;
+using Assets.Scripts.ui;
 
 public class WindowsManager : MonoBehaviour
 {
@@ -11,34 +12,28 @@ public class WindowsManager : MonoBehaviour
     public GameObject Logo;
     public GameObject Loading;
     public GameObject GameLogoBackground;
+    public AudioSource Source;
 
-    public bool loadingStopPlay;
-
-    private int currentLevel = 0;
+    public bool levelCanLoad;
 
     void Start()
     {
-        //Scene main = SceneManager.GetSceneByName("main");
-        //SceneManager.SetActiveScene(main);
-
         Logo.GetComponentInChildren<ButtonScript>().ActionDelegate += LoadSceneMain;
         Intro.SetActive(true);
         Logo.SetActive(false);
         Loading.SetActive(false);
         GameLogoBackground.SetActive(false);
         var introVideo = Intro.GetComponentInChildren<VideoPlayer>();
-        //var texture = new RenderTexture(1920, 1080, 1);
-        //introVideo.targetTexture = texture;
-        //introVideo.transform.parent.GetComponentInChildren<RawImage>().texture = texture;
         introVideo.Play();
         introVideo.loopPointReached += ShowGameIntro;
-
-        //ShowLogoTest();
     }
 
     public void ShowGameIntro(VideoPlayer vp)
     {
         vp.Stop();
+        //PreloaderAnimator.Instance.Play("Start_Level");
+
+        StartCoroutine(FadeIn(Source, 0.5f));
         Intro.SetActive(false);
         Logo.SetActive(true);
         var gameIntro = Logo.GetComponentInChildren<VideoPlayer>();
@@ -50,12 +45,6 @@ public class WindowsManager : MonoBehaviour
         gameIntro.loopPointReached += ShowLogo;
     }
 
-    //public void ShowLogoTest()
-    //{
-    //    Intro.SetActive(false);
-    //    Logo.SetActive(true);
-    //}
-
     public void ShowLogo(VideoPlayer vp)
     {
         vp.Stop();
@@ -66,9 +55,9 @@ public class WindowsManager : MonoBehaviour
 
     public void LoadSceneMain()
     {
+        //PreloaderAnimator.Instance.Play("Inbetween");
         var videoFinishIsSet = false;
         Logo.SetActive(false);
-        currentLevel = 1;
         Loading.SetActive(true);
         var loadingVideo = Loading.GetComponentInChildren<VideoPlayer>();
         var texture = new RenderTexture(1920, 1080, 1);
@@ -79,8 +68,9 @@ public class WindowsManager : MonoBehaviour
     }
     public void FinishLoading(VideoPlayer vp)
     {
-        loadingStopPlay = true;
         vp.Stop();
+        StartCoroutine(FadeOut(Source, 0.5f));
+        //PreloaderAnimator.Instance.Play("Inbetween");
     }
 
     public IEnumerator LoadScene(VideoPlayer vp, bool videoFinishIsSet)
@@ -96,13 +86,45 @@ public class WindowsManager : MonoBehaviour
                     vp.loopPointReached += FinishLoading;
                     videoFinishIsSet = true;
                 }
-                if (loadingStopPlay)
+                if (levelCanLoad)
                 {
                     asyncLoad.allowSceneActivation = true;
                 }
             }
             yield return null;
         }
+    }
+
+    public IEnumerator FadeOut(AudioSource audioSource, float FadeTime)
+    {
+        float startVolume = audioSource.volume;
+
+        while (audioSource.volume > 0)
+        {
+            audioSource.volume -= startVolume * Time.deltaTime / FadeTime;
+
+            yield return null;
+        }
+
+        audioSource.Stop();
+        audioSource.volume = startVolume;
+        levelCanLoad = true;
+    }
+
+    public IEnumerator FadeIn(AudioSource audioSource, float FadeTime)
+    {
+        float startVolume = 0.2f;
+
+        audioSource.volume = 0;
+        audioSource.Play();
+
+        while (audioSource.volume < 1.0f)
+        {
+            audioSource.volume += startVolume * Time.deltaTime / FadeTime;
+
+            yield return null;
+        }
+        audioSource.volume = 1f;
     }
 
 }
