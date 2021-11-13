d77ec98d8af772bc355637f806a16400a54540ea
Artur Korzh <korzh.artur@gmail.com>
2021-11-13 10:02:09 +0200

    set enemy speed

--- YouAreNotTheMainHero/Assets/Scripts/AnimationSpeed.cs
+++ YouAreNotTheMainHero/Assets/Scripts/AnimationSpeed.cs
@@ -1,16 +1,18 @@
-using System.Collections;
 using UnityEngine;
 
+[RequireComponent(typeof(Enemy))]
 public class ExampleScript : MonoBehaviour
 {
     public Animation anim;
 
     void Start()
     {
+        var enemy = GetComponent<Enemy>();
+
         // Walk backwards
-        anim["Base_pers_anim_bastet"].speed = -1.0f;
+        anim["Base_pers_anim_bastet"].speed = enemy.Speed * 0.25f;
 
         // Walk at double speed
-        anim["Base_pers_anim_bastet"].speed = 2.0f;
+        anim["Base_pers_anim_bastet"].speed = enemy.Speed * 0.25f;
     }
 }
