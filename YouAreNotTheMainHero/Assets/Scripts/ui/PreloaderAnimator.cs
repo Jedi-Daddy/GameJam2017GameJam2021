e021ba29a624c83195afeae3a627f9b438536755
VShev <viky2503@gmail.com>
2021-11-14 02:30:43 +0200

    test preloader

---
+++ YouAreNotTheMainHero/Assets/Scripts/ui/PreloaderAnimator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.ui
+{
+    public class PreloaderAnimator : MonoBehaviour
+    {
+        public static PreloaderAnimator Instance;
+        private Animator _animator;
+
+        void Awake()
+        {
+            Instance = this;
+            _animator = GetComponentInChildren<Animator>();
+        }
+
+        public void Play(string name)
+        {
+            _animator.SetTrigger(name);
+        }
+    }
+}
